aea33f9abb1f2d1d2d79ebba59038839103123f8
hemant <hemant_chaudhari@persistent.co.in>
2016-08-17 13:12:58 +0530

    Committing VMS Application

---
+++ src/UserExistException.java
@@ -0,0 +1,16 @@
+
+
+public class UserExistException extends Exception {
+	
+	public UserExistException(){
+		
+		super("Exception");
+		
+	}
+	public UserExistException(String msg){
+		
+	super(msg);
+	}
+
+
+}
